0fffccfc6ca8d08ad3adec11f65488e8f5e50347
mjmjeong <rlaalwjd0520@gmail.com>
2025-03-16 14:12:34 +0000

    add front_video / side_video

--- main_debug.py
+++ main_debug.py
@@ -10,44 +10,92 @@ from utils import *
 mp_pose = mp.solutions.pose
 
 def evaluate_pose(args):
-    # Youtube video
-    angle_min = []
-    angle_min_hip = []
-    cap = cv2.VideoCapture(args.video_path)
-
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
-    size = (640, 480) # TODO
+    # init evaluator
     evaluator = get_pose_evaluator(args.workout)
+
+    # read front video
+    cap_front = cv2.VideoCapture(args.front_video_path)
+    width_front = int(cap_front.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+    height_front = int(cap_front.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+
+    # read side video
+    cap_side = cv2.VideoCapture(args.side_video_path)
+    width_side = int(cap_side.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+    height_side = int(cap_side.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
     
+    # videos (outputs)
+    if args.save_path: 
+        size = (640, 480) # TODO: change! 
+        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+        video_frames = cv2.VideoWriter(args.save_path, fourcc, 24, size)
+
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-        while cap.isOpened():
-            ret, frame = cap.read()
+        while cap_front.isOpened():
+            #################################
+            # step 1: front
+            #################################
+            ret, frame = cap_front.read()
+            if not ret:
+                break
             if frame is not None:
                 frame_ = rescale_frame(frame, percent=75)
             
-            # Recolor image to RGB
-            image = cv2.cvtColor(frame_, cv2.COLOR_BGR2RGB)
-            image.flags.writeable = False
-        
-            # Make detection
-            results = pose.process(image)
-        
-            # Recolor back to BGR
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # measure
+            front_image = cv2.cvtColor(frame_, cv2.COLOR_BGR2RGB)
+            front_image.flags.writeable = False
+            results = pose.process(front_image)
+            evaluator.front_update(front_image, results)
 
-            observation = evaluator.get_observation(image, results)
-            if args.debug: 
-                break            
-            break
-            breakpoint()
-    breakpoint()
+            # drawing
+            front_image.flags.writeable = True
+            front_image = cv2.cvtColor(front_image, cv2.COLOR_RGB2BGR)
+            evaluator.front_drawing(front_image, results)
+            front _image.flags.writeable = False
 
+            #################################
+            # step 2: side
+            #################################
+            ret, frame = cap_side.read()
+            if not ret:
+                break
+            if frame is not None:
+                frame_ = rescale_frame(frame, percent=75)
+            
+            # measure
+            side_image = cv2.cvtColor(frame_, cv2.COLOR_BGR2RGB)
+            side_image.flags.writeable = False
+            results = pose.process(side_image)
+            evaluator.side_update(side_image, results)
+
+            # drawing
+            side_image.flags.writeable = True
+            side_image = cv2.cvtColor(side_image, cv2.COLOR_RGB2BGR)
+            evaluator.front_drawing(side_image, results)
+            side_image.flags.writeable = False
+
+            ########################################
+            # step 3: aggregate
+            ########################################
+            full_image = evaluator.build_screen(front_image, side_image)
+            cv2.imshow('Evaluating your pose!', full_image)
+            video_frames.write(image)
+    
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                cap_front.release()
+                cap_side.release()
+                video_frames.release()
+                cv2.destroyAllWindows()
+
+    cap_front.release() # detach allocate 
+    cap_side.release() # detach allocate 
+    cv2.destroyAllWindows()
+    # saving local video
+   
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--save_path", default='output.mp4')
-    parser.add_argument("--video_path", default='youTube_video.mp4')
+    parser.add_argument("--front_video_path", default='youTube_video.mp4')
+    parser.add_argument("--side_video_path", default='youTube_video.mp4')
     parser.add_argument("--workout", default='squat')
     
     args = parser.parse_args()
--- modules/squat.py
+++ modules/squat.py
@@ -9,39 +9,65 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 class SquatEvaluator:
     def __init__(self):
-        self.count = 0
-        # criteria
-        self.min_ang = 1000
+        # current state: front
+        self.front_counter = 0
+        self.front_stage = "ongoing"
+        self.min_ang_knee = 1000
         self.min_ang_hip = 10000
 
-        # Curl counter variables
-        counter = 0 
-        min_ang = 0
-        max_ang = 0
-        min_ang_hip = 0
-        max_ang_hip = 0
-        stage = None
+        # current state: side
+        self.side_counter = 0 # due to missync issue
+        self.side_stage = "ongoing" # due to missync issue
+        self.side_min_ang_knee = 1000
+        self.side_min_ang_hip = 10000
+
+        # total criteria
+        self.command = ""
+        self.overall_criteria = {
+            "criteria_1": False,
+            "criteria_2": False,
+            "criteria_3": False,
+            "criteria_4": False,
+            }
+
+    def front_update(self, results):
+        front_observation = self.get_front_obsevation(results)
+        curr_knee_angle = front_observation["knee_angle"]
+        curr_hip_angle = front_observation["hip_angle"]
+
+        # ongoing: stack data
+        if self.front_stage == 'ongoing': 
+            self.angles_knee.append(curr_knee_angle)
+            self.angles_hip.append(curr_hip_angle)
+            
+        # check_finish
+        flag_finish = curr_knee_angle > min(self.angles_knee) and curr_knee_angle <= 90 
+        if angle_knee <= 90 and self.front_stage =='ongoing':
+            self.front_stage="finish"
+            self.counter +=1
+            # criteria 1: 
+            min_angle_knee  = min(anngles_knee)
+            if min_angle_knee > 0: 
+                self.overall_criteria['criteria_1'] = True
+            else:
+                self.overall_criteria['criteria_1'] = False # reset / or keep? 
         
-    def get_observation(self, results):
-        """
-        input: results from mediapipe
-        output: target_dict
-        """
+        if angle_knee > 169: # start next iteration
+            self.front_stage = "ongoing"
+
+   def get_front_observation(self, results):
         # Extract landmarks
-        landmarks = results.pose_landmarks.landmark
-        
+        landmarks = results.pose_landmarks.landmark        
         # Get coordinates
         shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
         elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
         wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-        
         
         # Get coordinates
         hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
         knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
         ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
     
-        
         # Calculate angle
         angle = calculate_angle(shoulder, elbow, wrist)
         
@@ -60,45 +86,24 @@ class SquatEvaluator:
                     }
         return observation_dict
 
-    def count(self, observation_dict):
-       # Curl counter logic
-        if angle_knee > 169:
-            self.stage = "up"
-        if angle_knee <= 90 and self.stage =='up':
-            self.stage="down"
-            counter +=1
-            print(counter)
-            min_ang  =min(self.angle_min)
-            max_ang = max(self.angle_min)
-            
-            min_ang_hip  =min(self.angle_min_hip)
-            max_ang_hip = max(self.angle_min_hip)
-            
-            print(min(angle_min), " _ ", max(angle_min))
-            print(min(angle_min_hip), " _ ", max(angle_min_hip))
-            angle_min = []
-            angle_min_hip = [] 
-
-    def render(self, image, results, observation_dict, save_video, save_path_per_img=None):
-        cv2.rectangle(image, (20,20), (435,160), (0,0,0), -1)
-        # Rep data
-        cv2.putText(image, "Repetition : " + str(self.counter), 
-                    (30,60), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-        # Stage data
+    def front_drawing(self, image, results): 
         cv2.putText(image, "Knee-joint angle : " + str(self.min_ang), 
-                    (30,100), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-        #Hip angle:
+                (30,100), # text position
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
         cv2.putText(image, "Hip-joint angle : " + str(self.min_ang_hip), 
-                    (30,140), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-        # Render detections
+                (30,140), # text position
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2), 
-                                mp_drawing.DrawingSpec(color=(203,17,17), thickness=2, circle_radius=2) 
-                                 )               
-        if save_path_per_img is not None: 
-            breakpoint()
-        else:
-            cv2_vieo.write(image)
+                        mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2), 
+                        mp_drawing.DrawingSpec(color=(203,17,17), thickness=2, circle_radius=2) 
+                        )               
+    
+    def build_screen(self, screen, front_image, side_image):
+        if self.front_stage = "up" and self.side_stage = "up": 
+            direction_command = self.get_direction()
+        return screen
+
+    def get_direction(self):
+        
+        return command
+        
